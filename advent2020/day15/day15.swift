afade0e3941ce18c83a97a05ecf651e61cc91a5a
Elliot Barer <ebarer@mac.com>
2020-12-14 22:21:24 -0800

    Day 15

--- advent2020/day15/day15.swift
+++ advent2020/day15/day15.swift
@@ -6,3 +6,48 @@
 //
 
 import Foundation
+
+struct Day15: Day {
+    let index: Int = 15
+    var input: [Any]
+    
+    init() {
+        input = AdventParser<String>(file: "day\(index).txt").inputs() ?? []
+    }
+    
+    func memoryGame(turns iterations: Int) -> Int {
+        let startingNumbers: [Int] = (input as! [String]).first!.split(separator: ",").map { Int($0)! }
+
+        var orderSpoken = [Int: [Int]]()
+        var lastNumber: Int = -1
+        for turn in 0..<iterations {
+            var num: Int = 0
+            if turn < startingNumbers.count {
+                num = startingNumbers[turn]
+            } else if let turns = orderSpoken[lastNumber], turns.count == 2 {
+                num = turns[0] - turns[1]
+            }
+            
+            if orderSpoken[num] == nil {
+                orderSpoken[num] = [turn, turn]
+            } else {
+                let tmp = orderSpoken[num]![0]
+                orderSpoken[num]![0] = turn
+                orderSpoken[num]![1] = tmp
+            }
+
+            lastNumber = num
+        }
+
+        return lastNumber
+    }
+    
+    func partOne() -> Any {
+        return memoryGame(turns: 2020)
+    }
+    
+    func partTwo() -> Any {
+        return memoryGame(turns: 30000000)
+    }
+}
+
--- advent2020/main.swift
+++ advent2020/main.swift
@@ -10,7 +10,7 @@ import Foundation
 let days: [Day] = [
                        Day1(),  Day2(),  Day3(),  Day4(),  Day5(),
      Day6(),  Day7(),  Day8(),  Day9(), Day10(), Day11(), Day12(),
-    Day13(), Day14()
+    Day13(), Day14(), Day15()
 ]
 
 func allDays() {
@@ -19,8 +19,6 @@ func allDays() {
 }
 
 func today() {
-    printHeader(title: false)
-    
     guard let start = DateComponents(calendar: Calendar.current, timeZone: TimeZone(abbreviation: "EST"), year: 2020, month: 12, day: 1).date,
           let dif = Calendar.current.dateComponents([.day], from: start, to: Date()).day,
           days.count > dif
@@ -29,7 +27,10 @@ func today() {
         return
     }
 
-    print(days[dif].description, "\n")
+    let description = days[dif].description
+    
+    printHeader(title: false)
+    print(description, "\n")
 }
 
 //allDays()
